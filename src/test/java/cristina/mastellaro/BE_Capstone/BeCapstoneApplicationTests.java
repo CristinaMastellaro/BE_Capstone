07cab187d24f4b7d81113ae639612f5de6194318
Cristina Mastellaro <cristinamastellaro8@gmail.com>
2025-11-03 09:54:47 +0100

    Initial commit

---
+++ src/test/java/cristina/mastellaro/BE_Capstone/BeCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package cristina.mastellaro.BE_Capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
