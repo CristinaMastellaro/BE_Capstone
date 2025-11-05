2a3534b579c8f30a25e8e4018a46c3ba7184d8e9
Cristina Mastellaro <cristinamastellaro8@gmail.com>
2025-11-05 14:20:50 +0100

    create repos

---
+++ src/main/java/cristina/mastellaro/BE_Capstone/repositories/MoodRepository.java
@@ -0,0 +1,11 @@
+package cristina.mastellaro.BE_Capstone.repositories;
+
+import cristina.mastellaro.BE_Capstone.entities.Mood;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface MoodRepository extends JpaRepository<Mood, UUID> {
+}
