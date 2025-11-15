8550b773b1e00f2aad67725d4e2d2e0f4428d589
Cristina Mastellaro <cristinamastellaro8@gmail.com>
2025-11-15 13:45:01 +0100

    final fix?

--- FE_Capstone/src/redux/actions/index.ts
+++ FE_Capstone/src/redux/actions/index.ts
@@ -2,7 +2,7 @@ import ShowSongType from "../../types/ShowSongType";
 import { AppDispatchFunction, IRootState } from "../store";
 
 // export const ENDPOINT = "http://localhost:8888";
-export const ENDPOINT = "https://becapstone-production.up.railway.app";
+export const ENDPOINT = "https://soundadventure-be.up.railway.app";
 
 // For login
 export const SET_USERNAME = "SET_USERNAME";
